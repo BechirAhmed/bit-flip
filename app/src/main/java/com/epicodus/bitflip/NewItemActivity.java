9c5f5459edf67f7d2f565f8ae8d44c6f8cb922e1
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-18 10:42:29 -0800

    add click functionality to newItemButton, making a toast when clicked

--- app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
+++ app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
@@ -2,23 +2,36 @@ package com.epicodus.bitflip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class NewItemActivity extends AppCompatActivity {
+public class NewItemActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.newItemDescription) EditText mNewItemDescription;
     @Bind(R.id.newItemName) EditText mNewItemName;
     @Bind(R.id.newItemPrice) EditText mNewItemPrice;
     @Bind(R.id.inputName) EditText mInputName;
     @Bind(R.id.inputEmail) EditText mInputEmail;
     @Bind(R.id.inputPhone) EditText mInputPhone;
+    @Bind(R.id.newItemButton) Button mNewItemButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
         ButterKnife.bind(this);
+
+        mNewItemButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mNewItemButton) {
+            Toast.makeText(NewItemActivity.this, "Item Saved!", Toast.LENGTH_LONG).show();
+        }
     }
 }
