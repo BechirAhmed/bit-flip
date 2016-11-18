4ea6512573710bf48715e3e463d6e34c12b923d9
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-18 13:57:20 -0800

    show chosen category in NewItemActivity

--- app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
+++ app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
@@ -1,10 +1,12 @@
 package com.epicodus.bitflip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.Bind;
@@ -14,6 +16,7 @@ public class NewItemActivity extends AppCompatActivity implements View.OnClickLi
     @Bind(R.id.newItemDescription) EditText mNewItemDescription;
     @Bind(R.id.newItemName) EditText mNewItemName;
     @Bind(R.id.newItemPrice) EditText mNewItemPrice;
+    @Bind(R.id.newItemCategory) TextView mNewItemCategory;
     @Bind(R.id.inputName) EditText mInputName;
     @Bind(R.id.inputEmail) EditText mInputEmail;
     @Bind(R.id.inputPhone) EditText mInputPhone;
@@ -25,6 +28,9 @@ public class NewItemActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_new_item);
         ButterKnife.bind(this);
 
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("category");
+        mNewItemCategory.setText("Category: " + category);
         mNewItemButton.setOnClickListener(this);
     }
 
