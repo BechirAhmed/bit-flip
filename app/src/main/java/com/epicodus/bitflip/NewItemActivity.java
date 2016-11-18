687812dbe5159a100d810c742ad786c61695d8ec
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-18 10:31:21 -0800

    bind all EditText boxes in NewItemActivity using butterknife

--- app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
+++ app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
@@ -2,12 +2,23 @@ package com.epicodus.bitflip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class NewItemActivity extends AppCompatActivity {
+    @Bind(R.id.newItemDescription) EditText mNewItemDescription;
+    @Bind(R.id.newItemName) EditText mNewItemName;
+    @Bind(R.id.newItemPrice) EditText mNewItemPrice;
+    @Bind(R.id.inputName) EditText mInputName;
+    @Bind(R.id.inputEmail) EditText mInputEmail;
+    @Bind(R.id.inputPhone) EditText mInputPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+        ButterKnife.bind(this);
     }
 }
