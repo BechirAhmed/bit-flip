0b2762b18420ea36e6e91aab3682a1d9dfa42200
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-22 09:48:14 -0800

    bind textviews to backend of ItemDisplayActivity using butterknife

--- app/src/main/java/com/epicodus/bitflip/ItemDisplayActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ItemDisplayActivity.java
@@ -2,13 +2,23 @@ package com.epicodus.bitflip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ItemDisplayActivity extends AppCompatActivity {
-
+    @Bind(R.id.itemName) TextView mItemName;
+    @Bind(R.id.itemDescription) TextView mItemDescription;
+    @Bind(R.id.itemPrice) TextView mItemPrice;
+    @Bind(R.id.userName) TextView mUserName;
+    @Bind(R.id.userPhone) TextView mUserPhone;
+    @Bind(R.id.userEmail) TextView mUserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_display);
+        ButterKnife.bind(this);
     }
 }
