3e729b58c526cb277044c7d511414e60d489c28e
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-11-22 10:18:09 -0800

    add spinner to NewItemActivity so user can choose a category

--- app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
+++ app/src/main/java/com/epicodus/bitflip/NewItemActivity.java
@@ -1,11 +1,14 @@
 package com.epicodus.bitflip;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,19 +19,25 @@ public class NewItemActivity extends AppCompatActivity implements View.OnClickLi
     @Bind(R.id.newItemDescription) EditText mNewItemDescription;
     @Bind(R.id.newItemName) EditText mNewItemName;
     @Bind(R.id.newItemPrice) EditText mNewItemPrice;
-    @Bind(R.id.newItemCategory) TextView mNewItemCategory;
+    @Bind(R.id.spinnerCategory) Spinner mNewItemCategory;
     @Bind(R.id.inputName) EditText mInputName;
     @Bind(R.id.inputEmail) EditText mInputEmail;
     @Bind(R.id.inputPhone) EditText mInputPhone;
     @Bind(R.id.newItemButton) Button mNewItemButton;
+    private String[] mCategoryArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
         ButterKnife.bind(this);
+        Resources res = getResources();
+
+        mCategoryArray = res.getStringArray(R.array.categories);
 
         mNewItemButton.setOnClickListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, mCategoryArray);
+        mNewItemCategory.setAdapter(adapter);
     }
 
     @Override
