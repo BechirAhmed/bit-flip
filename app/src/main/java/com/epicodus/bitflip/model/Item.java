81a44dbfc3d36d527bb8cbfb2476a9c09e7d120e
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-01 20:34:44 -0800

    add getSimilarItems function to ComparePricesActivity

---
+++ app/src/main/java/com/epicodus/bitflip/model/Item.java
@@ -0,0 +1,8 @@
+package com.epicodus.bitflip.model;
+
+/**
+ * Created by DroAlvarez on 12/1/16.
+ */
+
+public class Item {
+}
--- app/src/main/java/com/epicodus/bitflip/ui/ComparePricesActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ui/ComparePricesActivity.java
@@ -1,15 +1,56 @@
 package com.epicodus.bitflip.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.epicodus.bitflip.R;
+import com.epicodus.bitflip.model.Item;
+import com.epicodus.bitflip.services.WalmartService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ComparePricesActivity extends AppCompatActivity {
+    private ArrayList<Item> mCompareItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_prices);
+
+        Intent intent = getIntent();
+        String item = intent.getStringExtra("itemName");
+
+        getSimilarItems(item);
+    }
+
+    private void getSimilarItems(String item) {
+        final WalmartService walmartService = new WalmartService();
+
+        walmartService.findItems(item, new Callback() {
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) {
+                mCompareItems = walmartService.processSearchResults(response);
+
+                ComparePricesActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+        });
     }
 }
