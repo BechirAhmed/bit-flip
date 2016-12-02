66bc6c057c1c88780da257ac763d4558ae2f4510
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-01 19:18:45 -0800

    add ui package, put walmart apikey in gradle.properties, setup Constant.java for future api call, and add gradle.properties to .gitignore

---
+++ app/src/main/java/com/epicodus/bitflip/Constants.java
@@ -0,0 +1,18 @@
+package com.epicodus.bitflip;
+
+import android.os.Build;
+
+/**
+ * Created by DroAlvarez on 12/1/16.
+ */
+
+public class Constants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String API_BASE_URL = "http://api.walmartlabs.com/v1/";
+    public static final String API_KEY_QUERY_PARAMETER = "apiKey";
+    public static final String FORMAT_QUERY_PARAMETER = "format";
+    public static final String FORMAT_QUERY_ANSWER = "json";
+    public static final String CATEGORY_URL_ADDON = "taxonomy";
+    public static final String SEARCH_URL_ADDON = "search";
+    public static final String SEARCH_QUERY_PARAMETER = "query";
+}
