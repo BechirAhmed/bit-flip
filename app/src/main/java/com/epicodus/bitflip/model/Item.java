066bafd37af546d55590920e22dbf94dafeaa639
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-02 09:34:46 -0800

    add Item model with name, price, description, and imageUrl

--- app/src/main/java/com/epicodus/bitflip/model/Item.java
+++ app/src/main/java/com/epicodus/bitflip/model/Item.java
@@ -5,4 +5,31 @@ package com.epicodus.bitflip.model;
  */
 
 public class Item {
+    private String mName;
+    private String mPrice;
+    private String mDescription;
+    private String mImageUrl;
+
+    public Item(String name, String price, String description, String imageUrl) {
+        this.mName = name;
+        this.mPrice = price;
+        this.mDescription = description;
+        this.mImageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
 }
