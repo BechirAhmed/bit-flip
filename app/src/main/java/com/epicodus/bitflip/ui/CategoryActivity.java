60ad9940181699a4d757d5c4211b4db668af909e
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-08 19:38:16 -0800

    inflate menu_search when icon is clicked

--- app/src/main/java/com/epicodus/bitflip/ui/CategoryActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ui/CategoryActivity.java
@@ -2,8 +2,13 @@ package com.epicodus.bitflip.ui;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,6 +32,9 @@ public class CategoryActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Resources res = getResources();
 
+        MenuItem menuItem = menu.fi
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView()
+
         mCategoryArray = res.getStringArray(R.array.categories);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mCategoryArray);
         mCategoryList.setAdapter(adapter);
@@ -37,4 +45,33 @@ public class CategoryActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search)
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchItems(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
 }
