81a7db15430ed38c964b828349b6c0904ebb8a13
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-09 11:07:30 -0800

    add findSearchedItems function to SearchItemsActivity, using the query from user to get item from firebase that match up with the query and displaying them in RecyclerView

--- app/src/main/java/com/epicodus/bitflip/adapters/FirebaseItemViewHolder.java
+++ app/src/main/java/com/epicodus/bitflip/adapters/FirebaseItemViewHolder.java
@@ -1,14 +1,23 @@
 package com.epicodus.bitflip.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.epicodus.bitflip.Constants;
 import com.epicodus.bitflip.R;
 import com.epicodus.bitflip.model.Item;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 /**
  * Created by DroAlvarez on 12/8/16.
@@ -42,6 +51,27 @@ public class FirebaseItemViewHolder extends RecyclerView.ViewHolder implements V
 
         nameTextView.setText(item.getName());
         categoryTextView.setText(item.getCategory());
-        priceTextView.setText(item.getPrice());
+        priceTextView.setText("$" + item.getPrice());
+    }
+
+    @Override
+    public void onClick(View v) {
+        final ArrayList<Item> items = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ITEMS);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    items.add(snapshot.getValue(Item.class));
+                }
+                int itemPosition = getLayoutPosition();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/epicodus/bitflip/ui/SearchItemsActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ui/SearchItemsActivity.java
@@ -3,25 +3,85 @@ package com.epicodus.bitflip.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.epicodus.bitflip.Constants;
 import com.epicodus.bitflip.R;
+import com.epicodus.bitflip.adapters.FirebaseItemViewHolder;
+import com.epicodus.bitflip.model.Item;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SearchItemsActivity extends AppCompatActivity {
     @Bind(R.id.searchItemsRecyclerView) RecyclerView mSearchItemsRecyclerView;
+
+    private DatabaseReference mItemReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_items);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String query = intent.getStringExtra("query");
 
-        getItems(query);
+        mItemReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_ITEMS);
+        findSearchedItems(query);
+
     }
 
-    private
+    private void findSearchedItems(final String query) {
+        mItemReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                final List<Item> items = new ArrayList<Item>();
+                for(DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+                    String itemName = itemSnapshot.child("name").getValue().toString();
+                    if(itemName.equals(query)) {
+                        Item item = (Item) itemSnapshot.getValue();
+                        items.add(item);
+                    }
+                }
+
+                setUpFirebaseAdapter();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Item, FirebaseItemViewHolder>(Item.class, R.layout.bitflip_list_item, FirebaseItemViewHolder.class, mItemReference) {
+            @Override
+            protected void populateViewHolder(FirebaseItemViewHolder viewHolder, Item model, int position) {
+                viewHolder.bindItem(model);
+            }
+        };
+        mSearchItemsRecyclerView.setHasFixedSize(true);
+        mSearchItemsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mSearchItemsRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
 }
