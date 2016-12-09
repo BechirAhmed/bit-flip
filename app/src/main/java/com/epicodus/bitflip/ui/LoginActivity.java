e754d8fe6940c8a81c2b0a5f01efda49bb77635e
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-08 17:54:55 -0800

    bind EditTexts and Button in layout with backend for LoginActivity

--- app/src/main/java/com/epicodus/bitflip/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ui/LoginActivity.java
@@ -5,15 +5,25 @@ import android.support.v4.content.IntentCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.epicodus.bitflip.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mRegisterTextView.setOnClickListener(this);
+        mPasswordLoginButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if(v == mRegisterTextView) {
+        if(v == mPasswordLoginButton) {
+            loginWithPassword();
+        } else if(v == mRegisterTextView) {
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
             finish();
