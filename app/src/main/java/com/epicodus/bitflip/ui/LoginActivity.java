d21a211e5b3f94d86e4cdc33f2c18d23460acc85
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-08 18:36:59 -0800

    add progress dialog when app is authenticating with firebase after user logs in through LoginActivity

--- app/src/main/java/com/epicodus/bitflip/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/bitflip/ui/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.bitflip.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.content.IntentCompat;
@@ -32,6 +33,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mRegisterTextView.setOnClickListener(this);
         mPasswordLoginButton.setOnClickListener(this);
+
+        createAuthProgressDialog();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -94,15 +98,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             return;
         }
 
+        mAuthProgressDialog.show();
+
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
                         if(!task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
     }
 
 
