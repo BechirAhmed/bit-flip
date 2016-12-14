aca34cca147503c00032fed2ffcdd5ca0bec0147
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-12-13 19:50:53 -0800

    add itemDetailActivity and itemDetailFragment so users can see more details on an item and swipe through the pictures

---
+++ app/src/main/java/com/epicodus/bitflip/ui/ItemDetailActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.bitflip.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.bitflip.R;
+
+public class ItemDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_detail);
+    }
+}
